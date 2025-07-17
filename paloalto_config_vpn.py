7978988d34447e4739fde6e6e942ec86e65b9a01
Rosner Nascimento <rosnerti@outlook.com>
2025-07-16 22:25:52 -0300

    feat: nova configuraçaõ da api

--- paloalto_config_vpn.py
+++ paloalto_config_vpn.py
@@ -1,31 +1,47 @@
 import requests
 
-# Configuração IKE Gateway no Palo Alto
-url = "http:/172.16.16.246/api/?type=config&action=set&key=API_KEY"
+#Configuracao API
+# Substitur por sua API Key
+IP_FIREWALL = "172.16.16.246"
+API_KEY = "SUA_API_KEY"  
+
+url = f"https://{IP_FIREWALL}/api/?type=config&action=set&key={API_KEY}"
+
 xml_payload = """
-<request>
-    <set>
-        <network>
-            <ike>
-                <gateway>
-                    <entry name="VPN_FORTIGATE">
-                        <protocol>
-                            <ikev2>
-                                <dpd>
-                                    <enable>yes</enable>
-                                </dpd>
-                            </ikev2>
-                        </protocol>
-                        <peer-address>203.0.113.1</peer-address>
-                        <pre-shared-key>S3nh4F0rt3!</pre-shared-key>
-                        <local-interface>ethernet1/1</local-interface>
-                        <ikev2-crypto-profile>VPN_PROFILE</ikev2-crypto-profile>
-                    </entry>
-                </gateway>
-            </ike>
-        </network>
-    </set>
-</request>
+<config>
+  <devices>
+    <entry name="lab.rosnertech">
+      <network>
+        <ike>
+          <gateway>
+            <entry name="VPN_FORTIGATE">
+              <authentication>
+                <pre-shared-key>
+                  <key>S3nh4F0rt3!</key>
+                </pre-shared-key>
+              </authentication>
+              <protocol>
+                <ikev2>
+                  <dpd>
+                    <enable>yes</enable>
+                  </dpd>
+                </ikev2>
+              </protocol>
+              <peer-address>203.0.113.1</peer-address>
+              <local-address>
+                <interface>ethernet1/1</interface>
+              </local-address>
+              <ikev2-crypto-profile>VPN_PROFILE</ikev2-crypto-profile>
+            </entry>
+          </gateway>
+        </ike>
+      </network>
+    </entry>
+  </devices>
+</config>
 """
+
 response = requests.post(url, data=xml_payload, verify=False)
-print(response.text)
+
+print("Status Code:", response.status_code)
+print("Resposta XML:\n", response.text)
